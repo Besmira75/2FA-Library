cc25becc2f6e426de582ea044e57b6d756371056
Astrit Krasniqi <121978549+astritkrasniqi1@users.noreply.github.com>
2024-05-19 21:10:37 +0200

    Created functions  sendEmail(), generateCode(), send2FACode(), verifyCode()
    Created functions for sending email, generating random code, sending this code in email and  to verify that code.

--- src/TwoFactorAuth.php
+++ src/TwoFactorAuth.php
@@ -23,5 +23,42 @@ class TwoFactorAuth
         $this->mailer->Port = $smtpPort;
         $this->fromEmail = $fromEmail;
     }
+
+    public function sendEmail($toEmail, $subject, $body)
+    {
+        try {
+            $this->mailer->setFrom($this->fromEmail);
+            $this->mailer->addAddress($toEmail);
+            $this->mailer->Subject = $subject;
+            $this->mailer->Body = $body;
+
+            $this->mailer->send();
+            return true;
+        } catch (Exception $e) {
+            error_log("Mailer Error: " . $this->mailer->ErrorInfo);
+            return false;
+        }
+    }
+
+    public function generateCode()
+    {
+        return rand(100000, 999999);
+    }
+
+    public function send2FACode($toEmail, $code)
+    {
+        $subject = "Your 2FA Code";
+        $body = "Your 2FA code is $code. It will expire in 5 minutes.";
+        return $this->sendEmail($toEmail, $subject, $body);
+    }
+
+    public function verifyCode($storedCode, $inputCode, $expiryTime)
+    {
+        $currentTime = new DateTime();
+        if ($currentTime < $expiryTime && $storedCode === $inputCode) {
+            return true;
+        }
+        return false;
+    }
 }
 ?>
